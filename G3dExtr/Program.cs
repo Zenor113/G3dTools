786b2afeab71a19c3941b77ab156615db913e776
David Birks <david@fourthgeneration.co.uk>
2021-09-05 00:42:18 +0100

    Add project files.

---
+++ G3dExtr/Program.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace G3dExtr
+{
+    class Program
+	{
+		//file variables
+		static int currentTexture;
+		static int textureTotal = 0;
+		static int fileSize;
+		static byte[] fileData;
+		static byte[] ddsHeader = Encoding.UTF8.GetBytes("DDS "); //in the g3d file these 4 bytes indicate the filesize, however in a DDS file the first 4 bytes need to be "DDS ".
+
+		static BinaryReader fileIn;
+		static void Main(string[] args)
+        {
+            //make directorys to save textures and meshes too 
+            Directory.CreateDirectory("textures");
+            Directory.CreateDirectory("meshes");
+
+
+
+            try
+            {
+                fileIn = new BinaryReader(File.OpenRead("AllMeshes.g3d"));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("could not open G3D file");
+                return;
+            }
+            if (new string(fileIn.ReadChars(8)).Equals("LiOnHeAd"))
+            {
+                fileIn.BaseStream.Seek(40, SeekOrigin.Current); //skip past rest of the file header info
+
+                
+                //===============
+                // High-res textures are first so we extract them
+                //===============
+                ExtractHighResTextures();
+                fileIn.BaseStream.Seek(-40, SeekOrigin.Current);
+
+                
+
+                if (fileIn.ReadString(4).Equals("INFO")) // Standard Black & White
+                {
+
+                    //===============
+                    // Mesh offsets come now, then the actual l3d data.
+                    //===============
+
+
+                    fileIn.BaseStream.Seek(4172, SeekOrigin.Current);
+                    ExtractMeshes("standardmesh.txt");
+                    //		currentOffset += (l3dLength + l3dLength);
+                    //		fileIn.Seek(currentOffset, ios::beg);
+
+                    //===============
+                    // Before finishing, extract the low res textures
+                    //===============
+                    ExtractLowResTextures();
+                }
+                else // Creature Isles
+                {
+                    fileIn.BaseStream.Seek(-4, SeekOrigin.Current);
+
+                    //===============
+                    // Low-res textures come now.
+                    //===============
+
+                    ExtractLowResTextures();
+
+                    //===============
+                    // Before finishing, extract the mesh offsets then the actual l3d data
+                    //===============
+                    fileIn.BaseStream.Seek(4176, SeekOrigin.Current);
+                    ExtractMeshes("creatureislemesh.txt");
+
+                }
+
+                Console.WriteLine("The contents of the G3D file have been successfully extracted to the \\meshes and \\textures folders.\n");
+            }
+            Console.WriteLine("Invalid G3D file");
+            // clean up
+            fileIn.Dispose();
+        }
+
+        private static void ExtractLowResTextures()
+        {
+            Console.WriteLine("Extracting low resolution ('-low') textures..");
+
+
+            //fileIn.seekg (3, SeekOrigin.Current);
+            string lowTexTag = "LOW";
+            currentTexture = textureTotal;
+            do
+            {
+
+                var test = string.Concat(fileIn.ReadChars(3));
+                if (!lowTexTag.Equals(test)) // find out the current texture number
+                {
+                    Console.WriteLine("invalid low textures");
+                    return;
+                }
+
+
+                fileIn.BaseStream.Seek(45, SeekOrigin.Current);
+                fileSize = fileIn.ReadInt32(); //read in the texture size
+
+                //fileData = new char[fileSize + 1]; //dimension memory to hold the texture data
+                fileData = fileIn.ReadBytes(fileSize - 4);
+
+                // ===== Find output dir/ name =====
+                string textureFile = string.Format("textures\\{0}-low.dds", currentTexture);
+
+                using (FileStream fileOut = File.OpenWrite(textureFile))
+                {
+                    fileOut.Write(ddsHeader, 0, 4);
+                    fileOut.Write(fileData, 0, fileSize - 4);
+
+                }
+
+                currentTexture--;
+                //currentOffset -= 4;
+
+            } while (currentTexture != 0);
+
+            Console.WriteLine("Completed.\n");
+        }
+
+        private static void ExtractMeshes(string meshnamefile)
+        {
+            Console.WriteLine("Extracting l3d meshes");
+
+            int numMeshes;
+            int[] meshOffset;
+            long meshInfoOffset;
+            string l3dHeader = "L3D0";
+            int l3dLength;
+
+            numMeshes = fileIn.ReadInt32(); //read in the texture size
+            meshInfoOffset = fileIn.BaseStream.Position;
+            meshInfoOffset -= 8;
+            //numMeshes = 704;
+
+            meshOffset = new int[numMeshes]; //dimension memory to hold mesh offsets
+
+            for (int meshRun = 0; meshRun < numMeshes; meshRun++)
+            {
+                meshOffset[meshRun] = fileIn.ReadInt32(); //read in the texture size
+            }
+
+            using (StreamReader fileHeader = File.OpenText(meshnamefile))
+            {
+
+                string meshName;
+                int mshCount = 0;
+                string meshNumber;
+
+                meshNumber = fileHeader.ReadLine();
+
+                //now read in the actual l3d data
+                for (long meshRun = 0; (meshRun) < numMeshes; meshRun++)
+                {
+
+                    //dps tuff for file header name
+                    mshCount++;
+                    meshName = fileHeader.ReadLine();
+                    //fileHeader >> meshName;
+                    //cout << "mesh" << mshCount << ": " << meshName << "\n";
+
+                    long currentOffset = meshInfoOffset + meshOffset[meshRun];
+                    var test = string.Concat(fileIn.ReadChars(4));
+                    if (!l3dHeader.Equals(test))
+                    {
+                        Console.WriteLine("ERROR! Could not find l3d header information.");
+                        return;
+                    }
+
+                    //fileIn.Seek(currentOffset, ios::beg);
+                    fileIn.BaseStream.Seek(4, SeekOrigin.Current); //move forward 4 bytes to get l3d size.
+                    l3dLength = fileIn.ReadInt32();
+                    //fileData = new char[l3dLength + 1]; //dimension memory to hold the l3d data
+                    fileIn.BaseStream.Seek(currentOffset, SeekOrigin.Begin); //now we return to the beginning of the l3d file (move back 8 bytes).
+                    fileData = fileIn.ReadBytes(l3dLength);
+
+                    // ===== Find output dir/ name =====
+                    string l3dFile = string.Format("meshes\\{0}.l3d", meshName);
+
+                    using (FileStream fileOut = File.OpenWrite(l3dFile))
+                    {
+                        fileOut.Write(fileData, 0, l3dLength);
+
+                    }
+
+                }
+            }
+            Console.WriteLine("Completed\n");
+        }
+
+        private static void ExtractHighResTextures()
+        {
+            Console.WriteLine("Extracting high resolution textures..");
+
+            do
+            {
+                currentTexture = fileIn.ReadInt32(); // find out the current texture number
+
+                if (textureTotal < currentTexture)
+                    textureTotal = currentTexture;
+
+                fileIn.BaseStream.Seek(4, SeekOrigin.Current);
+                fileSize = fileIn.ReadInt32(); //read in the texture size
+                                               //			fileSize -=4;
+
+                //fileData = new char[fileSize + 1]; //dimension memory to hold the texture data
+
+                fileData = fileIn.ReadBytes(fileSize - 4);
+
+                // ===== Find output dir/ name =====
+
+                string textureFile = string.Format("textures\\{0}.dds", currentTexture);
+
+                using (FileStream fileOut = File.OpenWrite(textureFile))
+                {
+                    fileOut.Write(ddsHeader, 0, 4);
+                    fileOut.Write(fileData, 0, fileSize - 4);
+
+                }
+                fileIn.BaseStream.Seek(40, SeekOrigin.Current);
+
+            } while (currentTexture != 1);
+
+            Console.WriteLine("Completed\n");
+        }
+    }
+    static class Utils
+    {
+        public static string ReadString(this BinaryReader br, int count)
+        {
+            return new string(br.ReadChars(count));
+        }
+    }
+}
---
+++ G3dPack/Program.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace G3dPack
+{
+    class Program
+    {
+        static bool CIflag = false;
+        static int numTex = 0;
+        static int[] textureInfo;
+        static string texturetxtfilename;
+        static string meshtxtfilename;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Make G3D\n");
+
+            Console.WriteLine("Checking mesh count");
+            try
+            {
+                var meshcount = Directory.GetFiles("meshes\\", "*.l3d").Length;
+                if (meshcount >= 704)// Creature Isle has 704 by default
+                {
+                    Console.WriteLine("Packing for Creature Isle");
+                    CIflag = true;
+                    texturetxtfilename = "creatureisletextures.txt";
+                    meshtxtfilename = "creatureislemesh.txt";
+                }
+                else if (meshcount >= 626) // Standard Black & White has 626 by default
+                {
+                    Console.WriteLine("Packing for standard Black & White");
+                    texturetxtfilename = "standardtextures.txt";
+                    meshtxtfilename = "standardmesh.txt";
+                }
+                else 
+                {
+                    Console.WriteLine("Error: not enough meshes, need minimum of 626");
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error accessing mesh folder");
+                return;
+            }
+
+            Console.WriteLine("[Linking High res textures]");
+            //Straight after the file header the next 4 bytes contain the texture number. ..
+            //therefore we retrieve the number of hi-res textures.
+            try
+            {
+                using (var fileIn = File.OpenText(texturetxtfilename))
+                {
+
+                    numTex = int.Parse(fileIn.ReadLine());
+                    textureInfo = new int[numTex];
+                    Console.WriteLine("there are: " + numTex + " textures");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not open file: '"+texturetxtfilename+"'\n..closing");
+                return; //Without the information the program will crash later on, so we'll close it now instead.
+            }
+
+            //Now we open the output file
+            using (var fileOut = new BinaryWriter(File.OpenWrite("AllMeshes.g3d")))
+            {
+                Console.WriteLine("file opened successfully");
+                //Now write the header
+                fileOut.Write(new[] { 'L', 'i', 'O', 'n', 'H', 'e', 'A', 'd' });
+                HighRes(fileOut);
+                //high res texture linking is complete
+
+                if (CIflag)
+                {
+                    LowRes(fileOut);
+                    Meshes(fileOut);
+                }
+                else
+                {
+                    Meshes( fileOut);
+                    LowRes( fileOut);
+                }
+
+
+            }
+        }
+
+        private static void LowRes(BinaryWriter fileOut)
+        {
+            for (int texLoop = numTex; texLoop > 0; texLoop--)
+            {
+
+                //next, we need to output the low res textures
+                char[] lowTexHeader = { 'L', 'O', 'W' };
+                fileOut.Write(lowTexHeader);
+
+                //write the number of textures
+                var temp = texLoop.ToString("x");
+                fileOut.Write(temp.ToCharArray());
+
+                //white space
+                fileOut.Write(new byte[29 - temp.Length]);
+
+                //Now read in the sequence of dds textures
+                try
+                {
+                    using (var fileIn = new BinaryReader(File.OpenRead(String.Format("textures\\{0}-low.dds", texLoop))))
+                    {
+
+
+                        //Find the length of the input file
+                        int fileEof = (int)fileIn.BaseStream.Length;
+                        fileEof += 12;
+                        //Setup and output texture length including the header section of 12 bytes
+                        fileOut.Write(fileEof);
+                        if (CIflag)
+                        {
+                            fileOut.Write(fileEof);
+                            fileOut.Write(texLoop);
+                            fileOut.Write(textureInfo[texLoop - 1]);
+                        }
+                        else
+                        {
+                            //more white space
+                            fileOut.Write(new byte[12]);
+                        }
+
+                        fileEof -= 12;
+                        fileOut.Write(fileEof);
+
+                        fileIn.BaseStream.Seek(4, SeekOrigin.Begin);
+                        fileOut.Write(fileIn.ReadBytes(fileEof));
+
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Could not open texture ID: " + texLoop + "\n\nclosing...");
+                    return;
+                }
+            }
+        }
+
+        private static void HighRes(BinaryWriter fileOut)
+        {
+            //Enter a texture loop
+            for (int texLoop = numTex; texLoop > 0; texLoop--)
+            {
+                //Write the texture number
+
+                // output the texture number
+                var temp = texLoop.ToString("x");
+                fileOut.Write(temp.ToCharArray());
+
+                //28 useless bytes go here, dont know why.
+                fileOut.Write(new byte[32 - temp.Length]);
+
+                //all other workings now need to be done after the input texture is opened
+                //Maybe the otehrs too - lcean up!
+
+                //Now read in the sequence of dds textures
+                try
+                {
+                    using (var fileIn = new BinaryReader(File.OpenRead(String.Format("textures\\{0}.dds", texLoop))))
+                    {
+
+                        //Find the length of the input file
+                        int fileEof = (int)fileIn.BaseStream.Length;
+                        fileEof += 12;
+                        //Setup and output texture length including the header section of 12 bytes
+                        fileOut.Write(fileEof);
+                        fileOut.Write(fileEof);
+
+                        fileOut.Write(texLoop);
+
+                        //Get and print texture type to file, also store type to an array for writing
+                        //in the info section
+
+                        fileIn.BaseStream.Seek(84, SeekOrigin.Begin);
+
+                        // Need to find out if the texture file is saved as DXT1 (type 1) or DXT3 (type 2)
+                        if (fileIn.ReadString(4).Equals("DXT3"))
+                        {
+                            textureInfo[texLoop - 1] = 2;
+                        }
+                        else
+                        {
+                            textureInfo[texLoop - 1] = 1;
+                        }
+
+                        //long texType;
+                        fileOut.Write(textureInfo[texLoop - 1]);
+
+                        fileEof -= 12;
+                        fileOut.Write(fileEof);
+
+                        fileEof -= 4;
+
+                        // need to check to see whether the dds file is 0 bytes
+
+                        fileIn.BaseStream.Seek(4, SeekOrigin.Begin);
+
+                        fileOut.Write(fileIn.ReadBytes(fileEof));
+
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Could not open texture ID: " + texLoop + "\n\nclosing...");
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("High res texture linking completed");
+        }
+
+        private static void Meshes(BinaryWriter fileOut)
+        {
+
+            //First comes the info header section
+            char[] infoHeader = { 'I', 'N', 'F', 'O' };
+            fileOut.Write(infoHeader);
+
+            //Just fill this with white space
+            fileOut.Write(new byte[28]);
+
+            //Length of INFO Section
+            int spaceLeft = 4100;
+            fileOut.Write(spaceLeft); // 4100 bytes in original
+
+            //before the actual texture info we write the number of textures
+            fileOut.Write(numTex);
+
+            spaceLeft -= 4;
+            for (int texLoop = numTex; texLoop > 0; texLoop--)
+            {
+
+                fileOut.Write(texLoop);
+                fileOut.Write(textureInfo[texLoop - 1]);
+
+                spaceLeft -= 8;
+            }
+            if (CIflag)
+            {
+                var temp = new byte[spaceLeft];
+                for (int i = 0; i < spaceLeft; i++)
+                {
+                    temp[i] = 0xCD;
+                }
+                fileOut.Write(temp);
+            }
+            else
+            {
+
+                //More white space
+
+                fileOut.Write(new byte[148]);
+
+                //quite a big heafer section here. Dont know what any of it does exactly though.
+                byte[] unknownHeader = { 132, 115, 98, 81, 00, 00, 00, 00, 39, 03, 131, 0, 1, 16, 0, 0, 120, 1, 194, 00, 120, 1, 194, 00 };
+
+                fileOut.Write(unknownHeader);
+                spaceLeft -= 172;
+
+                //now some more empty bytes.
+                fileOut.Write(new byte[spaceLeft]);
+            }
+
+            /////////////////////////////////////////////////
+            // Mesh header section then the mesh data
+            /////////////////////////////////////////////////
+
+            char[] meshHeader = { 'M', 'E', 'S', 'H', 'E', 'S' };
+            fileOut.Write(meshHeader);
+
+            // yet more bytes of nothing in particular
+            fileOut.Write(new byte[26]);
+
+            //Length of mesh section
+            if (CIflag)
+                fileOut.Write(0x7BFDC2);
+            else
+                fileOut.Write(0x6A351A);
+
+            char[] smallMeshHeader = { 'M', 'K', 'J', 'C' };
+            fileOut.Write(smallMeshHeader);
+
+            string strNumMesh;
+            //		char strOffset[255];
+            int numberOfMeshes;
+            int[] meshOffsets;
+            string meshName;
+            int sumOffset = 0;
+
+            //Now we work out the offsest, first open the mesh list
+            try
+            {
+                using (var fileIn = File.OpenText(meshtxtfilename))
+                {
+                    strNumMesh = fileIn.ReadLine();
+                    numberOfMeshes = int.Parse(strNumMesh);
+
+                    meshOffsets = new int[numberOfMeshes];
+                    //meshName = new char[numberOfMeshes];
+                    fileOut.Write(numberOfMeshes);
+
+                    // The first mesh offset is the size of the offset data block.
+                    meshOffsets[numberOfMeshes - 1] = ((numberOfMeshes * 4) + 8);
+                    fileOut.Write(meshOffsets[numberOfMeshes - 1]);
+                    sumOffset = meshOffsets[numberOfMeshes - 1];
+
+                    for (long meshNum = (numberOfMeshes - 1); meshNum > 0; meshNum--)
+                    {
+
+                        meshName = fileIn.ReadLine();
+
+                        try
+                        {
+
+
+
+                            using (var fileMesh = File.OpenRead(string.Format("meshes\\{0}.l3d", meshName)))
+                            {
+
+                                meshOffsets[meshNum - 1] = ((int)fileMesh.Length + meshOffsets[meshNum]);
+                                //sumOffset += meshOffsets[meshNum];
+
+                                fileOut.Write(meshOffsets[meshNum - 1]);
+
+                            }
+                        }
+
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Cannot open mesh file:" + meshName + ".l3d");
+                            return;
+                        }
+
+                    }
+
+
+                }
+            }
+            catch (Exception)
+
+            {
+                Console.WriteLine("Could not open file: '"+ meshtxtfilename + "'\n..closing");
+                return; //Without the information the program will crash later on, so we'll close it now instead.
+            }
+
+
+            //Straight after the offsets are printed, we print the actual mesh info.
+            try
+            {
+                using (var fileIn = File.OpenText(meshtxtfilename))
+                {
+                    fileIn.ReadLine();
+
+                    for (int meshNum = numberOfMeshes; meshNum > 0; meshNum--)
+                    {
+
+                        meshName = fileIn.ReadLine();
+                        try
+                        {
+                            fileOut.Write(File.ReadAllBytes(string.Format("meshes\\{0}.l3d", meshName)));
+                        }
+                        catch (Exception)
+                        {
+
+                            Console.WriteLine("Cannot open mesh file:" + meshName + ".l3d");
+                            return;
+                        }
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not open "+ meshtxtfilename + "...\nquitting program.");
+                return;
+            }
+        }
+    }
+
+    static class Utils
+    {
+        public static string ReadString(this BinaryReader br, int count)
+        {
+            return new string(br.ReadChars(count));
+        }
+    }
+}
